40b089f2b3df68fcd3b5643b9140a3deb7ab69f0
Mykael Mello <eng.mykael@gmail.com>
2024-10-01 15:25:39 -0300

    debug ok

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -3,6 +3,7 @@ import * as momentTimezone from 'moment-timezone';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { Logger } from '@nestjs/common';
+import { log } from 'console';
 
 const logger = new Logger('Main');
 
@@ -17,5 +18,12 @@ async function bootstrap() {
   await app.listen(8080);
 
   logger.log(`Api-Gateway is listening on port 8080`);
+
+  // Se o modo de depuração estiver ativo, inicie o debugger manualmente
+  if (process.env.ENV === 'dev') {
+    const inspector = await import('inspector');
+    inspector.open(9229, '0.0.0.0'); // Abra o debugger na porta 9229 para todas as interfaces
+    logger.log('Debugger listening on port 9229');
+  }
 }
 bootstrap();
